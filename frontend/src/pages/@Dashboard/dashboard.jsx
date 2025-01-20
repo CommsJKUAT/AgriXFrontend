6a0099382d99f2b684cc48aba840ac1cda5e4828
franfreezy <frandelwanjawa@yahoo.com>
2025-01-20 08:16:11 +0300

    update

--- frontend/src/pages/@Dashboard/dashboard.jsx
+++ frontend/src/pages/@Dashboard/dashboard.jsx
@@ -163,19 +163,20 @@ const Dashboard = () => {
                 </div>
               </a>
             </li>
+           
+            <li className="bg-transparent">
+              <SoilMoistureTemperatureChart />
+            </li>
             <li>
               <button
                 onClick={handleLocationSubmit}
-                className="px-4 py-2 mt-4 text-white bg-blue-600 rounded-lg hover:bg-blue-800"
+                className="px-4 py-2 mt-2 text-white bg-blue-600 rounded-lg hover:bg-blue-800"
               >
-                Use My Location
+                Set GS
               </button>
               {submitMessage && (
                 <p className="mt-2 text-sm">{submitMessage}</p>
               )}
-            </li>
-            <li className="bg-transparent">
-              <SoilMoistureTemperatureChart />
             </li>
           </ul>
         </div>
