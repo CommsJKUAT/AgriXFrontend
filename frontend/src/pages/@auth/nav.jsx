e3105813b77ae0aeeccf0a3ff41b469e68a02804
franfreezy <frandelwanjawa@yahoo.com>
2025-04-12 20:08:11 +0300

    update the logo position

--- frontend/src/pages/@auth/nav.jsx
+++ frontend/src/pages/@auth/nav.jsx
@@ -1,8 +1,8 @@
 function Nav() {
   return (
-    <nav class="2xl:fixed w-full top-0 left-0">
-      <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
+    <nav class="2xl:fixed w-full top-0 left-10">
+      <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-0 pl-0 p-4">
+        <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse ml-0">
           <img
             src="/satlogo1.jpeg"
             alt="AgroXSAT Logo"
